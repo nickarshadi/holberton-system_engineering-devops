c736eec8a3ec1327aa19c8034eab7cd7e41b8070
nickarshadi <1477@holbertonschool.com>
2020-12-08 08:37:08 -0500

    Add puppet file.

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#Modify ssh_config file.
+file_line { ' Deactivate password authentication.':
+  ensure => 'present',
+  path   => 'etc/ssh/ssh_config'
+  line   => 'PasswordAuthentication no'
+}
+
+file_line { ' Set Identity File.':
+  ensure => 'present',
+  path   => 'etc/ssh/ssh_config'
+  line   => 'IdentityFile ~/.ssh/holberton'
+}
