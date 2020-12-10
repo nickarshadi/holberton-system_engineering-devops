b00952ecd29abd386a242efa10c763c506bd2aa0
nickarshadi <1477@holbertonschool.com>
2020-12-09 19:20:15 -0500

    Add Puppet file.

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,37 @@
+Install and Setup NGINX server via puppet.
+exec { 'Update system':
+  command => '/usr/bin/env apt-get -y update',
+}
+
+exec { 'Install NGINX.':
+  command => '/usr/bin/env apt-get -y install nginx',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+}
+
+file_line { 'Sed append line':
+  ensure   => 'present',
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => '	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  multiple => true,
+}
+
+file_line { 'Sed append line':
+  ensure   => 'present',
+  path     => '/etc/nginx/sites-available/default',
+  after    => '	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line     => '	error_page 404 /custom_404.html;',
+  multiple => true,
+}
+
+
+file { '/var/www/html/custom_404.html':
+  content => 'Ceci n\'est pas une page',
+}
+
+exec { 'Starting NGINX':
+  command => '/usr/bin/env service nginx start',
+}
