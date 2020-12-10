4b1400325dc05f996fb729ac29b58442e31a974f
nickarshadi <1477@holbertonschool.com>
2020-12-09 19:33:47 -0500

    Add 404 page.

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -19,14 +19,7 @@ file_line { 'Sed append 301':
   multiple => true,
 }
 
-file_line { 'Sed append 404':
-  ensure   => 'present',
-  path     => '/etc/nginx/sites-available/default',
-  after    => '	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  line     => '	error_page 404 /custom_404.html;',
-  multiple => true,
-}
-
+exec {'/usr/bin/env sed -i "/server_name _;/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default': }
 
 file { '/var/www/html/custom_404.html':
   content => 'Ceci n\'est pas une page',
