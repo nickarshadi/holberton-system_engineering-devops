42cca94c7e16c25825df655361ed9160d45a9b76
nickarshadi <1477@holbertonschool.com>
2021-02-01 20:42:29 +0000

    Finish project.

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,23 +3,24 @@
 
 import json
 import requests
-from sys import argv
 
 if __name__ == '__main__':
-    userId = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(userId), verify=False).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(userId), verify=False).json()
-    username = user.get("username")
-    tasks = []
+    users = requests.get("https://jsonplaceholder.typicode.com/users",
+                         verify=False).json()
+    userdict = {}
+    usernamedict = {}
+    for user in users:
+        uid = user.get("id")
+        userdict[uid] = []
+        usernamedict[uid] = user.get("username")
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos",
+                        verify=False).json()
     for task in todo:
-        task_dict = {}
-        task_dict['task'] = task.get("title")
-        task_dict['completed'] = task.get('completed')
-        task_dict['username'] = username
-        tasks.append(task_dict)
-    jsonobj = {}
-    jsonobj[userId] = tasks
-    with open("{}.json".format(userId), 'w') as jsonfile:
-        json.dump(jsonobj, jsonfile)
+        taskdict = {}
+        uid = task.get("userId")
+        taskdict["task"] = task.get("title")
+        taskdict["completed"] = task.get("completed")
+        taskdict["username"] = usernamedict.get(uid)
+        userdict.get(uid).append(taskdict)
+    with open("todo_all_employees.json", 'w') as jsonfile:
+        json.dump(userdict, jsonfile)
